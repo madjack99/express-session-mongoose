3fe8d3621b2d141718cfc36abb7655331f8853db
madjack <madjack@nm.ru>
2019-06-10 17:02:42 +0400

    add reactstrap layout for Register

--- client/src/components/Register.js
+++ client/src/components/Register.js
@@ -1,8 +1,68 @@
 import React, { Component } from "react";
+import { Form, FormGroup, Label, Input, Button, Container } from "reactstrap";
 
 export class Register extends Component {
+  state = {
+    name: "",
+    email: "",
+    password: "",
+    loggedUser: null,
+    error: null
+  };
+
+  handleChange = e => {
+    this.setState({
+      [e.target.name]: e.target.value
+    });
+  };
+
+  handleSubmit = e => {
+    e.prevent.default();
+  };
+
   render() {
-    return <div>Register</div>;
+    return (
+      <div>
+        <Container>
+          <Form onSubmit={this.handleSubmit}>
+            <FormGroup>
+              <Label for="nameInput">Name</Label>
+              <Input
+                type="text"
+                name="name"
+                id="nameInput"
+                required
+                value={this.state.name}
+                onChange={this.handleChange}
+              />
+            </FormGroup>
+            <FormGroup>
+              <Label for="emailInput">Email</Label>
+              <Input
+                type="email"
+                name="email"
+                id="emailInput"
+                value={this.state.email}
+                onChange={this.handleChange}
+              />
+            </FormGroup>
+            <FormGroup>
+              <Label for="passwordInput">Password</Label>
+              <Input
+                type="password"
+                name="password"
+                id="passwordInput"
+                value={this.state.password}
+                onChange={this.handleChange}
+              />
+            </FormGroup>
+            <Button className="bg-success" block>
+              Regiser
+            </Button>
+          </Form>
+        </Container>
+      </div>
+    );
   }
 }
 
