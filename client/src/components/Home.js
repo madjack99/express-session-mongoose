7b5c0d65dc7dbb22b3573a6c9c9a15f0f4e3fb4e
madjack <madjack@nm.ru>
2019-06-11 17:38:59 +0400

    add reactstrap Modal to Home

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -1,5 +1,75 @@
-import React from "react";
+import React, { Component } from "react";
+import {
+  Container,
+  Button,
+  Modal,
+  ModalHeader,
+  ModalBody,
+  ModelFooter,
+  Form,
+  FormGroup,
+  Label,
+  Input
+} from "reactstrap";
 
-export default function Home() {
-  return <div>Home</div>;
+export default class Home extends Component {
+  state = {
+    modal: false,
+    title: "",
+    text: ""
+  };
+
+  toggle = () => {
+    this.setState({
+      modal: !this.state.modal
+    });
+  };
+
+  handleChange = e => {
+    this.setState({
+      [e.target.id]: e.target.value
+    });
+  };
+
+  handleSubmit = e => {
+    e.preventDefault();
+    console.log(this.state);
+    this.toggle();
+  };
+
+  render() {
+    return (
+      <Container>
+        <h1 className="bg-success p-2 mt-1 rounded text-center">
+          Login to create post
+        </h1>
+        <Button color="danger" onClick={this.toggle}>
+          CreatePost
+        </Button>
+        <Modal isOpen={this.state.modal} toggle={this.toggle}>
+          <ModalHeader toggle={this.toggle}>Create you post</ModalHeader>
+          <Container>
+            <Form onSubmit={this.handleSubmit}>
+              <FormGroup>
+                <Label for="title">Title</Label>
+                <Input id="title" required onChange={this.handleChange} />
+              </FormGroup>
+              <FormGroup>
+                <Label for="text">Text</Label>
+                <Input
+                  type="textarea"
+                  id="text"
+                  required
+                  onChange={this.handleChange}
+                />
+              </FormGroup>
+              <Button className="bg-danger mb-2 " block>
+                Create post
+              </Button>
+            </Form>
+          </Container>
+        </Modal>
+      </Container>
+    );
+  }
 }
